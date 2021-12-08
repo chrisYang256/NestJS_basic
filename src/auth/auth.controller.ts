df7b48264f08d1a451ae7969ebb5f2193ada8e06
chris-y <maxsummer256@gmail.com>
2021-12-08 23:50:02 +0900

    add: passport strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
@@ -15,5 +16,13 @@ export class AuthController {
     @Post('/signIn')
     signIn(@Body() AuthCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         return this.authService.signIn(AuthCredentialsDto)
+    }
+
+    @Post('/authTest')
+    // Guard는 인증 미들웨어 입니다.
+    // UseGuards() 안에 @Nestjs/passport에서 가져온 AuthGuard()를 이용하면 요청 안에 유저 정보를 넣어줄 수 있습니다.
+    @UseGuards(AuthGuard())
+    authTest(@Req() req) {
+        console.log('req:::', req);
     }
 } 
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,44 @@
+import { ConfigService } from "@nestjs/config";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+
+import { User } from "./user.entity";
+import { UserRepository } from "./user.repository";
+
+// Nest는 의존성 주입 시스템(Dependency Injection system)을 통해 이 서비스를 필요로하는 어디서든 inject 할 수 있습니다.
+@Injectable()
+// 이 클래스는 @nestjs/passport package를 통해 정의된 PassportStrategy 클래스를 확장합니다.
+// passport-jwt Node.js package를 통해 정의된 JWT Strategy를 통과합니다.
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        // 토큰이 유효한지 확인 후 payload 안에 들어있는 유저 이름을 통해 DB에서 유저정보를 가져오기 위해 UserRepository를 가져옵니다.
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository,
+        private readonly configService: ConfigService
+    ) {
+        // 2가지 중요한 옵션을 전달합니다.
+        super({
+            // 토큰이 유효한지 체크합니다.
+            // JWT Token 해독을 위해 JWT Stragegy가 사용할 시크릿키를 구성하고 Token에 접근합니다.
+            secretOrKey: process.env.JWT_SECRET,
+            // 토큰을 헤더에서 받아와서 위의 시크릿키로 유효한지 확인합니다.
+            // Bearer token으로 전달된 현재 요청의 인증 헤더에서 JWT를 찾도록 Strategy(imported from passport-jwt package)를 설정합니다.
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+        })
+    }
+
+    // 위에서 토큰이 유효한지 체크가 되면 validate method에서 payload에 있는 유저이름이 DB에 있는 유저인지 확인 후
+    // 있다면 유저 객체를 return값으로 던져주고 return값은 @UseGuards(AuthGuard())를 이용한 모든 요청의 Request Object에 들어갑니다.
+    async validate(payload) { 
+        const { name } = payload;
+        const user: User = await this.userRepository.findOne({ name });
+
+        if(!user) {
+            throw new UnauthorizedException(`Unauthorized user:${user}`);
+        }
+
+        return user;
+    }
+}
